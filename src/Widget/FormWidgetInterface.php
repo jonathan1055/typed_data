fd7daa56abf6539ea84931a1e79699f6e19890a2
Tim Rohaly <tr@202830.no-reply.drupal.org>
2019-12-04 19:40:07 +0000

    Issue #3092000 by TR: ConfigurablePluginInterface is deprecated in core 8.7.x

--- src/Widget/FormWidgetInterface.php
+++ src/Widget/FormWidgetInterface.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\typed_data\Widget;
 
-use Drupal\Component\Plugin\ConfigurablePluginInterface;
+use Drupal\Component\Plugin\ConfigurableInterface;
 use Drupal\Component\Plugin\PluginInspectionInterface;
 use Drupal\Core\Form\SubformStateInterface;
 use Drupal\Core\TypedData\DataDefinitionInterface;
@@ -12,7 +12,7 @@ use Symfony\Component\Validator\ConstraintViolationListInterface;
 /**
  * Interface definition for form widget plugins.
  */
-interface FormWidgetInterface extends ConfigurablePluginInterface, PluginInspectionInterface {
+interface FormWidgetInterface extends ConfigurableInterface, PluginInspectionInterface {
 
   /**
    * Returns if the widget can be used for the provided data.
